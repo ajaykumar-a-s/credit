8aacb4eabb68e46b4d2054be598d7c82de51da83
SAMYUKTHA KRP <krpsamyuktha2002@gmail.com>
2024-02-22 11:26:25 +0530

    service method declaration

--- src/main/java/com/project/credit/customer/service/CustomerServiceImpl.java
+++ src/main/java/com/project/credit/customer/service/CustomerServiceImpl.java
@@ -3,10 +3,34 @@ package com.project.credit.customer.service;
 import com.project.credit.customer.entity.Customer;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CustomerServiceImpl implements CustomerService{
     @Override
     public Customer saveCustomer(Customer customer) {
         return null;
     }
+
+
+    @Override
+    public Customer getCustomerById(Long customerId) {
+        return null;
+    }
+    @Override
+    public Customer editCustomer(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public Customer deleteCustomer (Long customerId)
+    {
+        return null;
+    }
+
+    public List<Customer> viewAllCustomer()
+    {
+        return null;
+    }
 }
+
