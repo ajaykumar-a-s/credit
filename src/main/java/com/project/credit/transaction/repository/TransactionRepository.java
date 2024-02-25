924c02b42022d248038776b144cd7c038f677c2d
Ajaykumar A S <asajay1902@gmail.com>
2024-02-25 20:48:28 +0530

    methods, api endpoint, exception handing

--- src/main/java/com/project/credit/transaction/repository/TransactionRepository.java
+++ src/main/java/com/project/credit/transaction/repository/TransactionRepository.java
@@ -1,7 +1,12 @@
 package com.project.credit.transaction.repository;
 
+import com.project.credit.card.entity.CreditCard;
 import com.project.credit.transaction.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findAllByCreditCard(CreditCard creditCard);
+    List<Transaction> findAllByDateBetween(java.sql.Date startDate, java.sql.Date endDate);
 }
