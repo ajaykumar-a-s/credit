b61748d25891131b641d6e46b7edc2eb1556f988
Surandharg <surandhar002@gmail.com>
2024-02-27 14:45:35 +0530

    add Transcation to bill

--- src/main/java/com/project/credit/bill/service/BillService.java
+++ src/main/java/com/project/credit/bill/service/BillService.java
@@ -3,6 +3,7 @@ package com.project.credit.bill.service;
 import com.project.credit.bill.entity.Bill;
 import com.project.credit.bill.exception.BillException;
 import com.project.credit.card.exception.CardException;
+import com.project.credit.transaction.entity.Transaction;
 import com.project.credit.transaction.exception.DateException;
 import com.project.credit.transaction.exception.TransactionException;
 
@@ -10,7 +11,7 @@ import java.util.List;
 
 public interface BillService {
     Bill autoGenerateBillForMonth(String cardNumber) throws BillException, TransactionException, DateException, CardException;
-    Bill billPayment(String cardNumber) throws BillException, CardException;
+    Bill billPayment(String cardNumber) throws BillException, CardException, TransactionException;
 
 }
 
